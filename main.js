0c3c3ad6408dbf2543f6769abeb1ad4103d2a2e9
Leonardo Caetano <leonardo.caetano@manypets.com>
2022-06-27 15:18:37 -0300

    fix: lint

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
       product: 'Socks',
       image: './assets/images/socks_green.jpg',
       inStock: true,
-      details: [ 
+      details: [
         '50% cotton',
         '30% wool',
         '20% polyester'
