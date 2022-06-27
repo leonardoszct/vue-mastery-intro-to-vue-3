37085dfa0cdc5869f8526fdd2a78550f2414ccf5
Leonardo Caetano <leonardo.caetano@manypets.com>
2022-06-27 16:21:30 -0300

    feat: components and props

--- main.js
+++ main.js
@@ -2,50 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       cart: 0,
-      product: 'Socks',
-      brand: 'Vue Mastery',
-      selectedVariant: null,
-      details: [
-        '50% cotton',
-        '30% wool',
-        '20% polyester'
-      ],
-      variants: [
-        { 
-          id: 2234, 
-          color: 'green', 
-          image: './assets/images/socks_green.jpg', 
-          quantity: 50 
-        },
-        { 
-          id: 2235, 
-          color: 'blue', 
-          image: './assets/images/socks_blue.jpg', 
-          quantity: 0 
-        }
-      ]
+      premium: true
     }
   },
-  beforeMount() {
-    this.updateVariant(0)
-  },
-  methods: {
-    addToCart() {
-      this.cart += 1
-    },
-    updateVariant(index) {
-      this.selectedVariant = this.variants[index]
-    }
-  },
-  computed: {
-    title() {
-      return `${this.brand} ${this.product}`
-    },
-    image() {
-      return this.selectedVariant?.image
-    },
-    inStock() {
-      return this.selectedVariant?.quantity
-    }
-  }
+  methods: { }
 })
