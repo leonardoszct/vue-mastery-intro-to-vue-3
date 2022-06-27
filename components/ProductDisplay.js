3a89990514bb14ad8e78b117b3f06d1d81b5c895
Leonardo Caetano <leonardo.caetano@manypets.com>
2022-06-27 16:27:41 -0300

    feat: communicating events

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -71,7 +71,7 @@ app.component('product-display', {
   },
   methods: {
     addToCart() {
-      this.cart += 1
+      this.$emit('add-to-cart', this.selectedVariant.id)
     },
     updateVariant(index) {
       this.selectedVariant = this.variants[index]
