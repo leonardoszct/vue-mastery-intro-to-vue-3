89ed4c9ea71b2ad5de37857732f5f6c7e115e949
Leonardo Caetano <leonardo.caetano@manypets.com>
2022-06-27 16:05:32 -0300

    feat: computed properties

--- main.js
+++ main.js
@@ -1,27 +1,51 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart:0,
+      cart: 0,
       product: 'Socks',
-      image: './assets/images/socks_green.jpg',
-      inStock: false,
+      brand: 'Vue Mastery',
+      selectedVariant: null,
       details: [
         '50% cotton',
         '30% wool',
         '20% polyester'
       ],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+        { 
+          id: 2234, 
+          color: 'green', 
+          image: './assets/images/socks_green.jpg', 
+          quantity: 50 
+        },
+        { 
+          id: 2235, 
+          color: 'blue', 
+          image: './assets/images/socks_blue.jpg', 
+          quantity: 0 
+        }
       ]
     }
+  },
+  beforeMount() {
+    this.updateVariant(0)
   },
   methods: {
     addToCart() {
       this.cart += 1
     },
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = this.variants[index]
+    }
+  },
+  computed: {
+    title() {
+      return `${this.brand} ${this.product}`
+    },
+    image() {
+      return this.selectedVariant?.image
+    },
+    inStock() {
+      return this.selectedVariant?.quantity
     }
   }
 })
